46b56e436766616bab2f3084d35e49d1c347580d
Johnny Johnson <johnny2136@gmail.com>
2024-08-30 10:17:05 -0400

    Added Code Comments

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"strconv"
 )
-
+//API interface that gets the value that will be passed to the Func fibonacci
 func fibonacciHandler(w http.ResponseWriter, r *http.Request) {
 	// Get the value of the 'n' query parameter
 	nStr := r.URL.Query().Get("n")
@@ -21,7 +21,7 @@ func fibonacciHandler(w http.ResponseWriter, r *http.Request) {
 	// Write the Fibonacci sequence as a response
 	fmt.Fprintf(w, "Fibonacci sequence up to %d: %v", n, result)
 }
-
+//Func fibonacci generates a fibonacci sequence based on "fibonacci(n)" passed from the handler
 func fibonacci(n int) []int {
 	sequence := make([]int, n)
 	if n > 0 {
